0419072e8eefce3282bd54f33069515acc6d91c4
Julian <julianl116910@gmail.com>
2025-09-24 22:30:33 -0500

    feat/GroupService ^ Observers: se agrego funcionalidad para consultar la capacidad de cada grupo/Julian :)

--- src/main/java/edu/dosw/sirha/service/GroupService.java
+++ src/main/java/edu/dosw/sirha/service/GroupService.java
@@ -1,12 +1,18 @@
 package edu.dosw.sirha.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 import edu.dosw.sirha.dto.request.GroupRequestDTO;
 import edu.dosw.sirha.dto.response.GroupResponseDTO;
 import edu.dosw.sirha.exception.ResourceNotFoundException;
 import edu.dosw.sirha.mapper.GroupMapper;
+import edu.dosw.sirha.mapper.ScheduleMapper;
 import edu.dosw.sirha.model.Group;
+import edu.dosw.sirha.model.observers.GroupObserver;
 import edu.dosw.sirha.repository.GroupRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -18,8 +24,9 @@ import lombok.extern.slf4j.Slf4j;
 public class GroupService {
 
     private final GroupRepository groupRepository;
-
+    private final List<GroupObserver> listeners;
     private final GroupMapper groupMapper;
+    private final ScheduleMapper scheduleMapper;
 
     @Transactional
     public GroupResponseDTO createRequest(GroupRequestDTO dto) {
@@ -38,19 +45,38 @@ public class GroupService {
 
         Group group = groupRepository.findByNumberGroup(numberGroup)
                 .orElseThrow(() -> ResourceNotFoundException.create("ID", numberGroup));
+        
+        //Creditos a actualizar
+        int oldQuotas = group.getAvailableQuotas();
 
         group.setNumberGroup(dto.getNumberGroup());
         group.setCapacity(dto.getCapacity());
         group.setAvailableQuotas(dto.getAvailableQuotas());
         group.setSubjectCode(dto.getSubjectCode());
         group.setUserId(dto.getUserId());
-        // group.setSchedules(dto.getSchedules());
+
+        if (dto.getSchedules() != null) {
+            group.setSchedules(
+            dto.getSchedules().stream()
+            .map(scheduleDto -> scheduleMapper.toEntity(scheduleDto)) 
+            .collect(Collectors.toCollection(ArrayList::new))
+            );
+        }
 
         Group saved = groupRepository.save(group);
-
+    
+        
+        //Implementacion Observer ? 
+        if(oldQuotas != saved.getAvailableQuotas()){
+            listeners.forEach(listener -> 
+                listener.updateAvailableCredits(saved.getNumberGroup(),oldQuotas,saved.getAvailableQuotas()));
+        }
         return groupMapper.toDto(saved);
 
     }
+
+
+
     /*
      * public void deleteGroup(String numberGroup){
      * if (!groupRepository.)
