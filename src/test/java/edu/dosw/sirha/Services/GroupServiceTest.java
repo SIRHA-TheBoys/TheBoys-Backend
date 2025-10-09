1ea4f7b5306250b41b700d6952b94023e8c85a57
Nestor David <nestor.lopez-c@mail.escuelaing.edu.co>
2025-10-08 20:18:00 -0500

    Feat: Pruebas controladores

--- src/test/java/edu/dosw/sirha/Services/GroupServiceTest.java
+++ src/test/java/edu/dosw/sirha/Services/GroupServiceTest.java
@@ -1,8 +1,16 @@
 package edu.dosw.sirha.Services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
+
+import edu.dosw.sirha.mapper.ScheduleMapper;
+import edu.dosw.sirha.mapper.UserMapper;
+import edu.dosw.sirha.model.entity.User;
+import edu.dosw.sirha.model.entity.enums.Role;
+import edu.dosw.sirha.model.observers.GroupObserver;
+import edu.dosw.sirha.repository.SubjectRepository;
+import edu.dosw.sirha.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,15 +19,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import edu.dosw.sirha.mapper.GroupMapper;
 import edu.dosw.sirha.model.dto.request.GroupRequestDTO;
-import edu.dosw.sirha.model.dto.request.UserRequestDTO;
 import edu.dosw.sirha.model.dto.response.GroupResponseDTO;
-import edu.dosw.sirha.model.dto.response.UserResponseDTO;
 import edu.dosw.sirha.model.entity.Group;
-import edu.dosw.sirha.model.entity.User;
-import edu.dosw.sirha.model.entity.enums.Career;
-import edu.dosw.sirha.model.entity.enums.Role;
 import edu.dosw.sirha.repository.GroupRepository;
 import edu.dosw.sirha.service.GroupService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class GroupServiceTest {
@@ -28,6 +35,24 @@ public class GroupServiceTest {
 
         @Mock
         private GroupMapper groupMapper;
+
+        @Mock
+        private UserMapper userMapper;
+
+        @Mock
+        private ScheduleMapper scheduleMapper;
+
+        @Mock
+        private UserRepository studentRepository;
+
+        @Mock
+        private SubjectRepository subjectRepository;
+
+        @Mock
+        private UserRepository userRepository;
+
+        @Mock
+        private List<GroupObserver> observers;
 
         @InjectMocks
         private GroupService groupService;
@@ -64,4 +89,57 @@ public class GroupServiceTest {
                 assertEquals("CALI", response.getSubjectCode());
                 assertEquals(25, response.getCapacity());
         }
+        @Test
+        void shouldUpgradeGroup(){
+            String numberGroup = "1";
+
+            GroupRequestDTO dto = GroupRequestDTO.builder()
+                    .numberGroup(numberGroup)
+                    .capacity(30)
+                    .availableQuotas(5)
+                    .subjectCode("FIEM")
+                    .build();
+
+            Group existingGroup = Group.builder()
+                    .numberGroup(numberGroup)
+                    .capacity(25)
+                    .availableQuotas(10)
+                    .subjectCode("CALI")
+                    .usersId(new ArrayList<>())
+                    .build();
+
+            Group updatedGroup = Group.builder()
+                    .numberGroup(numberGroup)
+                    .capacity(30)
+                    .availableQuotas(5)
+                    .subjectCode("FIEM")
+                    .usersId(new ArrayList<>())
+                    .build();
+            GroupResponseDTO responseDTO = GroupResponseDTO.builder()
+                    .numberGroup(numberGroup)
+                    .capacity(30)
+                    .availableQuotas(5)
+                    .subjectCode("FIEM")
+                    .build();
+            when(groupRepository.findById(numberGroup)).thenReturn(Optional.of(existingGroup));
+            when(groupRepository.save(existingGroup)).thenReturn(updatedGroup);
+            when(groupMapper.toDto(updatedGroup)).thenReturn(responseDTO);
+            GroupResponseDTO result = groupService.updateGroup(numberGroup, dto);
+            assertEquals("FIEM", result.getSubjectCode());
+            assertEquals(30, result.getCapacity());
+            assertEquals(5, result.getAvailableQuotas());
+        }
+    @Test
+    void shouldDeleteGroup() {
+        when(groupRepository.existsById("999")).thenReturn(true);
+
+        groupService.deleteGroup("999");
+
+        verify(groupRepository).deleteById("999");
+    }
+
+
+
+
+
 }
